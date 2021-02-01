8e6cedc687b6890f0fe78d872e79d90818aa8acc
Rick Nagy <rick.c.nagy@gmail.com>
2021-01-31 23:57:44 -0500

    Initial commit

---
+++ hw1/hello.cpp
@@ -0,0 +1,13 @@
+// A very simple C++ program
+// Author: <your name>
+// Date: <today's date>
+
+#include <iostream> // Needed for output to screen
+
+// Every C++ program needs the main function
+int main()
+{
+   std::cout << "Hello, World!\n"; // output "Hello, Word!"
+
+   return 0;  // indicate program ended successfully
+} // end of main()
